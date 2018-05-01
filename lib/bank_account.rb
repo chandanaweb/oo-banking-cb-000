a8446611e14cd7437423bc57b78cf2db55750e35
chandana M <chandu11288@gmail.com>
2018-05-01 00:53:37 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
-
+ attr_accessor  :balance, :status
+ attr_reader :name
+ def initialize(name, balance = 1000, status ="open")
+   @name = name
+   @balance = balance
+   @status = status
+ end
+ def deposit(amount)
+   @balance += amount
+ end
+ def display_balance
+   return "Your balance is $#{self.balance}."
+ end
+ def valid?
+   if self.balance < 1 || self.status == "closed"
+     return false
+   else
+     return true
+   end
+ end
+ def close_account
+   self.status = "closed"
+ end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
-  # your code here
+ attr_accessor :sender, :receiver , :status, :amount
+ @@accounts = []
+ def initialize(sender, receiver,amount =50, status ="pending")
+   @sender = BankAccount.new(sender)
+   @receiver = BankAccount.new(receiver)
+   @status = status
+   @amount = amount
+   @@accounts << self
+ end
+ def valid?
+   sender = BankAccount.new(@sender)
+   sender.valid?
+   #receiver = BankAccount.new(@receiver)
+   #receiver.valid?
+  # @@accounts.each do|account|
+    # return true
+   #end
+ end
+ def execute_transaction
+  sender = BankAccount.new(@sender)
+  sender.balance = self.amount
+ end
 end
